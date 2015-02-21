529bf90d2deeda7e998599616295b8e1f3ccdc1a
Renzo Carbonara <gnuk0001@gmail.com>
2015-02-21 16:26:54 -0300

    Initial

---
+++ src/example-ping-pong-cli/Main.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Main (main) where
+
+import           Control.Applicative
+import qualified Control.Concurrent.Async as Async
+import qualified Control.Concurrent.MVar as MVar
+import qualified Control.Exception as Ex
+import           Control.Monad (forever, forM_, guard, void)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Foldable (traverse_)
+import qualified Lei
+import           Prelude
+
+--------------------------------------------------------------------------------
+-- THE MODEL
+
+-- | The state of our application.
+data PingPongState = PingPongState
+  { _appStatus      :: !PingPong
+  , _appChangesLeft :: !Integer
+  , _appErrors      :: ![Error]
+  , _appStop        :: !Bool
+  } deriving (Eq, Show)
+
+-- | @'def' n@ create an 'PingPongState' starting on a 'Ping' state that allows up to
+-- @n@ changes, where @n > 0@.
+def :: Integer -> Maybe PingPongState
+def n = do
+    guard $ n > 0
+    return $ PingPongState Ping n [] False
+
+
+data PingPong = Ping | Pong
+  deriving (Eq, Show, Read)
+
+pingPongToggle :: PingPong -> PingPong
+pingPongToggle = \case
+    Ping -> Pong
+    Pong -> Ping
+
+
+data Error
+  = ErrorNoChangesLeft
+  | ErrorCantSwap !PingPong
+  deriving (Eq, Show)
+
+
+-- | Operations that will, undeniably, change our 'PingPongState'.
+data Op
+  = OpAddError Error
+  | OpClearErrors
+  | OpSwapStatus
+  | OpStop
+  deriving (Show)
+
+model :: Lei.Model Op PingPongState
+model = Lei.mkModel $ \o s -> case o of
+    OpStop -> s { _appStop = True }
+    OpAddError e -> s { _appErrors = e : _appErrors s }
+    OpClearErrors -> s { _appErrors = [] }
+    OpSwapStatus -> s { _appStatus = pingPongToggle $ _appStatus s
+                      , _appChangesLeft = _appChangesLeft s - 1
+                      }
+
+--------------------------------------------------------------------------------
+-- THE VIEW
+
+view :: Monad m => Env -> Lei.View () Req PingPongState m (IO ())
+view e = Lei.mkView_ $ \stopIO reqIO s -> do
+    printReport s
+    envPrompter e $ do a <- readPingPong
+                       reqIO $ ReqSetStatus a
+  where
+    msg :: String -> IO ()
+    msg a = putStrLn $ "| " ++ a
+
+    printReport :: PingPongState -> IO ()
+    printReport s = do
+       forM_ (_appErrors s) $ \e ->
+          msg $ "ERROR: " ++ errorHumanReadable e
+       msg $ "Current status: " ++ show (_appStatus s)
+       msg $ "Number of status changes left: " ++ show (_appChangesLeft s)
+       msg $ "Try typing " ++ show (pingPongToggle (_appStatus s))
+
+    readPingPong :: IO PingPong
+    readPingPong = do
+       ma <- readLnMay
+       case ma of
+          Just a  -> return a
+          Nothing -> do
+             msg $ "Oops, that's unacceptable, try typing Ping or Pong instead."
+             readPingPong
+
+errorHumanReadable :: Error -> String
+errorHumanReadable = \case
+    ErrorNoChangesLeft -> "You can't make any more changes."
+    ErrorCantSwap a -> "You can't switch to " ++ show a ++ " now."
+
+
+-- | This is supposed to be an “environment” providing tools that are
+-- requiered to properly render the view and such. In this case, it is a simple
+-- “UI toolkit” providing a tool that repeatedly prompts the user for something.
+data Env = Env
+  { envStop :: IO ()
+  , envPrompter :: IO () -> IO ()
+  }
+
+newEnv :: IO Env
+newEnv = do
+   lock <- MVar.newMVar ()
+   mvAsyncId <- MVar.newEmptyMVar
+   let killPrompter = traverse_ Async.cancel =<< MVar.tryTakeMVar mvAsyncId
+       stop = MVar.withMVar lock $ \() ->
+         killPrompter
+       prompter m = MVar.withMVar lock $ \() -> do
+         killPrompter
+         MVar.putMVar mvAsyncId =<< Async.async (forever m)
+   return (Env stop prompter)
+
+--------------------------------------------------------------------------------
+-- THE CONTROLLER
+
+-- | Requests that can be made to our application.
+data Req
+  = ReqStop
+  | ReqSetStatus !PingPong
+  deriving (Show)
+
+controller :: Monad m => Lei.Controller r0 o0 Req Op PingPongState m
+controller = Lei.mkController $ \s -> \case
+    ReqStop -> Lei.op OpStop
+    ReqSetStatus dst -> do -- TODO: MonadPlus instance for Lei.C
+      Lei.op OpClearErrors
+      let errors = concat
+            [ [ErrorNoChangesLeft | _appChangesLeft s < 1]
+            , [ErrorCantSwap Ping | _appStatus s /= pingPongToggle dst]
+            ]
+      case errors of
+        [] -> Lei.op OpSwapStatus
+        _  -> forM_ errors $ Lei.op . OpAddError
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    Just s0 <- return $ def 5
+    venv <- newEnv
+    Lei.run Ex.bracket s0 model controller (view venv)
+
+--------------------------------------------------------------------------------
+-- Internal tools
+
+readLnMay :: Read a => IO (Maybe a)
+readLnMay = do
+   l <- getLine
+   return $ case reads l of
+     [(a,"")] -> Just a
+     _        -> Nothing
---
+++ src/lib/Lei.hs
@@ -0,0 +1,353 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Lei
+  ( run
+
+  -- * Model
+  , Model
+  , mkModel
+
+  -- * Controller
+  , Controller
+  , mkController
+
+  , C
+  , req
+  , op
+  , stop
+  , bury
+  , nestController
+  , nestController0
+
+  -- * View
+  , View
+  , mkView
+  , mkView_
+  , nestView
+
+  , ViewStop
+  , mkViewStop
+
+  , ViewRender
+  , runViewRender
+
+  ) where
+
+import           Control.Applicative
+import qualified Control.Exception as Ex
+import           Control.Concurrent.MVar as MVar
+import           Control.Concurrent.Async as Async
+import qualified Control.Concurrent.STM as STM
+import           Control.Monad (void, unless)
+import           Control.Monad.IO.Class (MonadIO(liftIO))
+import           Control.Monad.Trans.Class (MonadTrans(lift))
+import qualified Control.Monad.Trans.State as State
+import           Data.Foldable (foldl', traverse_)
+import           Data.Function (fix)
+import           Data.Functor.Identity (Identity(..))
+import           Data.Monoid (Monoid(..))
+import           Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import qualified Pipes
+import           Pipes.Core ((//>))
+import           Prelude hiding (sequence_)
+
+
+--------------------------------------------------------------------------------
+
+newtype Model o s = Model { runModel :: o -> s -> s }
+
+mkModel :: (o -> s -> s) -> Model o s
+mkModel = Model
+
+--------------------------------------------------------------------------------
+
+-- | A 'Controller', constructed with 'controller', is where you deal with a
+-- concrete request of type @r@ using a given @'C' r0 o0 r o m a@.
+newtype Controller r0 o0 r o s m
+  = Controller { unController :: s -> r -> C r0 o0 r o m () }
+
+instance Monad m => Monoid (Controller r0 o0 r o s m) where
+  mempty = Controller $ \_ _ -> return ()
+  mappend a b = Controller $ \s r -> unController a s r >> unController b s r
+
+mkController :: (s -> r -> C r0 o0 r o m ()) -> Controller r0 o0 r o s m
+mkController = Controller
+
+-- | Run a 'Controller'.
+runController
+  :: Monad m
+  => Controller r0 o0 r o s m
+  -> (r -> r0)
+  -> (o -> o0)
+  -> s
+  -> r
+  -> Pipes.Producer (Maybe r0) m (Seq o0)
+runController cer r2r0 o2o0 s r = Pipes.hoist
+    (flip State.evalStateT mempty)
+    (runC (unController cer s r) r2r0 o2o0 >> lift State.get)
+
+--------------------------------------------------------------------------------
+
+-- | A @'C' r0 o0 r o m a@ is where you can issue requests of type @r@ and
+-- operations of type @o@, within a larger context of top-level requests of type
+-- @r0@ possibly leading to the issue of top-level operations of type @o0@'. A
+-- 'C' where the top-level request and operation types match the request
+-- and operation types of the current layer is said to be a top-level 'C'.
+-- A 'C' needs be converted into a 'Controller' using 'controller' before
+-- it can be used as (or as part of) a top-level 'Controller'.
+--
+-- * A request is delivered asyncrhonously for another 'Controller' to
+--   eventually c it. You use 'req' to issue a request.
+--
+-- * An operation is delivered synchronously for a model to update. You use 'op'
+--   to issue an operation.
+--
+-- 'Controller's can be nested inside a 'C' using the 'nest' and 'nest0'
+-- combinators, and actions to be executed at the current controller layer can
+-- be used by nested 'Controller's after being 'bury'ed.
+newtype C r0 o0 r o m a = C
+  { runC :: (r -> r0)
+         -> (o -> o0)
+         -> Pipes.Producer (Maybe r0) (State.StateT (Seq o0) m) a }
+  deriving (Functor)
+
+instance Monad m => Applicative (C r0 o0 r o m) where
+  pure a = C $ \_ _ -> pure a
+  mf <*> ma = C $ \r2r0 o2o0 ->
+      runC mf r2r0 o2o0 <*> runC ma r2r0 o2o0
+
+instance Monad m => Monad (C r0 o0 r o m) where
+  return a = C $ \ _ _ -> return a
+  ma >>= k = C $ \r2r0 o2o0 -> do
+      a <- runC ma r2r0 o2o0
+      runC (k a) r2r0 o2o0
+
+instance MonadTrans (C r0 o0 r o) where
+  lift ma = C $ \_ _ -> lift (lift ma)
+
+instance MonadIO m => MonadIO (C r0 o0 r o m) where
+  liftIO = lift . liftIO
+
+-- | Issue a local request for another 'Controller' to eventually handle it.
+req :: Monad m => r -> C r0 o0 r o m ()
+req r = C $ \r2r0 _ -> Pipes.yield $ Just (r2r0 r)
+
+-- | Issue a local operation for a model to update.
+-- All the given operations will be applied atomically.
+op :: Monad m => o -> C r0 o0 r o m ()
+op o = C $ \_ o2o0 -> lift $ State.modify (Seq.|> o2o0 o)
+
+stop :: Monad m => C r0 o0 r o m ()
+stop = C $ \_ _ -> Pipes.yield Nothing
+
+-- | Bury a 'C' so that it can be used at a lower 'C' layer
+-- sharing the same top-level request and operation types.
+bury
+  :: Monad m
+  => C r0 o0 r o m a
+  -> C r0 o0 r o m (C r0 o0 r' o' m a)
+bury c = C $ \r2r0 o2o0 -> return (C $ \_ _ -> runC c r2r0 o2o0)
+
+-- | Nest a 'Controller' compatible with the same top-level request
+-- and operation types.
+nestController
+  :: Monad m
+  => s
+  -> r
+  -> (r -> r')
+  -> (o -> o')
+  -> Controller r0 o0 r o s m
+  -> C r0 o0 r' o' m ()
+nestController s r r2r' o2o' cer = C $ \r'2r0 o'2o0 ->
+    runC (unController cer s r) (r'2r0 . r2r') (o'2o0 . o2o')
+
+-- | Nest a top-level 'Controller'.
+nestController0
+  :: (Monad m, Functor m)
+  => s
+  -> r
+  -> Controller r o r o s m
+  -> C r0 o0 r o m ()
+nestController0 s r cer = C $ \r2r0 o2o0 -> Pipes.hoist
+    (\x -> State.StateT $ \os' -> do
+       (a, os) <- State.runStateT x mempty
+       return $ (,) a $! mappend os' (fmap o2o0 os))
+    (runC (unController cer s r) id id //> Pipes.yield . fmap r2r0)
+
+--------------------------------------------------------------------------------
+
+newtype ViewRender v r s x = ViewRender
+  { runViewRender :: IO () -> (r -> IO ()) -> s -> v -> (x, v) }
+
+mkViewRender
+  :: (IO () -> (r -> IO ()) -> s -> State.State v x)
+  -> ViewRender v r s x
+mkViewRender k = ViewRender $ \stopIO reqIO s v ->
+                    State.runState (k stopIO reqIO s) v
+
+--------------------------------------------------------------------------------
+
+data View v r s m x = View !(ViewInit v m (v, ViewStop v, ViewRender v r s x))
+
+runView :: Monad m => View v r s m x -> m (v, ViewStop v, ViewRender v r s x)
+runView (View vi0) = do
+   ((v0, vs0, vr0), vs) <- runViewInit vi0
+   return (v0, mappend vs vs0, vr0)
+
+mkView
+  :: Monad m
+  => ViewInit v m (v,
+                   v -> IO (),
+                   IO () -> (r -> IO ()) -> s -> State.State v x)
+  -> View v r s m x -- ^
+mkView vi = View $ do
+  (v, iovs, k) <- vi
+  return (v, mkViewStop iovs, mkViewRender k)
+
+-- Like 'mkView', except for when there is no view state, initialization nor
+-- finalization to worry about.
+mkView_
+ :: Monad m
+ => (IO () -> (r -> IO ()) -> s -> x)
+ -> View () r s m x -- ^
+mkView_ k =
+    let vs = \() -> return ()
+        vr = \stopIO reqIO s -> return $! k stopIO reqIO s
+    in mkView $ return ((), vs, vr)
+
+--------------------------------------------------------------------------------
+
+newtype ViewInit v m a = ViewInit (State.StateT (ViewStop v) m a)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadTrans)
+
+runViewInit :: ViewInit v m a -> m (a, ViewStop v)
+runViewInit (ViewInit s) = State.runStateT s mempty
+
+nestView
+ :: Monad m
+ => Lens' v' v
+ -> View v r s m x
+ -> ViewInit v' m (v, ViewRender v' r s x)
+nestView l avw = ViewInit $ do
+   (av, avs, avr) <- lift $ runView avw
+   State.modify $ mappend (contramapViewStop (lensView l) avs)
+   let vr = ViewRender $ \vs' reqIO s v' ->
+         let (x, v) = runViewRender avr vs' reqIO s (lensView l v')
+         in (x, lensSet l v v')
+   return (av, vr)
+
+--------------------------------------------------------------------------------
+
+-- | Run a Lei application.
+--
+run
+  :: forall r o v s m
+   . (MonadIO m, Eq s)
+  => (forall x y z . m x -> (x -> m y) -> (x -> m z) -> m z)
+  -- ^ 'Control.Exception.bracket'-like function.
+  --    Hint: use 'Control.Monad.Catch.bracket' from "Control.Monad.Catch".
+  -> s
+  -- ^ Initial model state.
+  -> Model o s
+  -- ^ /Model/ update function. See 'model'.
+  -> Controller r o r o s m
+  -- ^ /Controller/ issuing controller requests and model operations.
+  -- See 'controller'.
+  -> View v r s m (IO ())
+     -- ^ /View/ issuing controller requests and rendering the model.
+  -> m ()
+run bracket s0 m cer vw = do
+    -- Channel through which requests are sent
+    tqR <- liftIO $ STM.newTQueueIO
+    let reqIO = STM.atomically . STM.writeTQueue tqR
+    -- TMVar where the last known model state is stored.
+    -- 'Right' means it needs rendering.
+    tmvSLast <- liftIO $ STM.newTMVarIO (Right s0)
+    -- TVar indicating whether to stop execution
+    tvStop <- liftIO $ STM.newTVarIO False -- TODO: a `MVar ()` should do
+    let stopIO = STM.atomically $ STM.writeTVar tvStop True
+    -- Handle an incomming requests
+    let handlerLoop :: m ()
+        handlerLoop = ($ s0) $ fix $ \loop s -> do
+           stopped <- liftIO $ STM.atomically $ STM.readTVar tvStop
+           unless stopped $ do
+              r <- liftIO $ STM.atomically $ STM.readTQueue tqR
+              os <- Pipes.runEffect $ do
+                 Pipes.for (runController cer id id s r) $ \mr' -> do
+                    case mr' of
+                       Just r' -> liftIO $ reqIO r'
+                       Nothing -> liftIO stopIO >> lift (loop s)
+              let !s' = foldl' (flip (runModel m)) s os
+              liftIO $ STM.atomically $ do
+                 void $ STM.tryTakeTMVar tmvSLast
+                 STM.putTMVar tmvSLast $! (if s == s' then Left else Right) s'
+              loop s'
+    -- Render the model
+    let renderLoop :: v -> ViewStop v -> ViewRender v r s (IO ()) -> IO ()
+        renderLoop v0 vs vr = ($ v0) $ fix $ \loop v -> do
+           flip Ex.onException (runViewStop vs v) $ do
+              stopped <- STM.atomically $ STM.readTVar tvStop
+              if stopped
+                 then runViewStop vs v
+                 else do
+                    Ex.bracketOnError
+                       (STM.atomically $ do
+                           es <- STM.takeTMVar tmvSLast
+                           case es of
+                              Left _ -> STM.retry
+                              Right s -> return s)
+                       (\s -> STM.atomically $ do
+                           void $ STM.tryPutTMVar tmvSLast $ Right s)
+                       (\s -> do
+                           let (io, v') = runViewRender vr stopIO reqIO s v
+                           io >> loop v')
+    bracket
+       (do (v, vs, vr) <- runView vw
+           liftIO $ flip Ex.onException (runViewStop vs v) $ do
+              a1 <- Async.async $ renderLoop v vs vr
+              a1 <$ Async.link a1)
+       (liftIO . Async.cancel)
+       (\a1 -> handlerLoop >> liftIO (Async.wait a1))
+
+
+--------------------------------------------------------------------------------
+-- Internal tools
+
+-- | A 'v -> IO ()' that can be ran just once. The next times you try to run it,
+-- it just returns @()@ without doing any actual work. It works across different
+-- threads and in case of exceptions.
+newtype ViewStop v = ViewStop (IO (MVar.MVar (Maybe (v -> IO ()))))
+
+mkViewStop :: (v -> IO ()) -> ViewStop v
+mkViewStop = ViewStop . MVar.newMVar . Just
+
+runViewStop :: MonadIO m => ViewStop v -> v -> m ()
+runViewStop (ViewStop iomv) v = liftIO $ do
+    mv <- iomv
+    Ex.bracket (MVar.takeMVar mv)
+               (\_ -> MVar.putMVar mv Nothing)
+               (traverse_ ($ v))
+
+-- | @'mappend' a b@ runs @a@ first and then @b@, even in case of exceptions.
+instance Monoid (ViewStop v) where
+  mempty = mkViewStop $ const $ return ()
+  mappend a b = mkViewStop $ \v -> runViewStop a v `Ex.finally` runViewStop b v
+
+contramapViewStop :: (a -> b) -> ViewStop b -> ViewStop a
+contramapViewStop f x = mkViewStop $ runViewStop x . f
+
+--------------------------------------------------------------------------------
+-- The wheel, mon ami, it is necessary to reinvent it.
+
+type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
+
+lensSet :: Lens' s a -> a -> s -> s
+lensSet l b = runIdentity . l (\_ -> Identity b)
+
+lensView :: Lens' s a -> s -> a
+lensView l = getConst . l Const
